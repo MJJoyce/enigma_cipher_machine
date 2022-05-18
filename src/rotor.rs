13baf200ec474439dab61ddc11581ceec9b81ce5
Michael Joyce <mltjoyce@gmail.com>
2022-05-17 21:44:49 -0700

    Simplify and clean up basic rotor implementation
    Minor code adjustments to simplify the general rotor implementation and
    clean up the code a bit. Rotor tyre configurations are now consts
    instead of the more complicated declaration approach tested previously.

--- src/rotor.rs
+++ src/rotor.rs
@@ -8,49 +8,52 @@ const ROTOR_2_ALPHABET: [char; 26] = [
     'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'
 ];
 
-//enum ROTOR_ALPHABET(
-
-    //I(['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O',
-        //'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J']),
-    //II(['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W',
-        //'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'])
-//)
-
 struct RotorTyre {
     notch: usize,
     alphabet: &'static [char; 26]
 }
 
-struct RotorI {
+const ROTOR_I: RotorTyre = RotorTyre {
+    // Rollover when 'Q' is in the "window" and moves to 'R'.
+    // Notch is actually aligned with 'Y'.
+    notch: 14,
+    alphabet: &ROTOR_1_ALPHABET
+};
+
+const ROTOR_II: RotorTyre = RotorTyre {
+    // Rollover when 'E' is in the "window" and moves to 'F'.
+    // Notch is actually aligned with 'M'
+    notch: 14,
+    alphabet: &ROTOR_2_ALPHABET
+};
+
+pub struct Rotor {
+    tyre: &'static RotorTyre,
     pos: usize,
-    ring_loc: usize,
-    tyre: RotorTyre
+    ring_loc: usize
 }
 
-impl RotorI {
-    fn new() -> Self {
-        return RotorI::new_with_state(0, 0)
+impl Rotor {
+    pub fn new(rotor_id: &str) -> Self {
+        Rotor::new_with_state(rotor_id, 0, 0)
     }
 
-    fn new_with_state(pos: usize, ring_loc: usize) -> Self {
-        RotorI {
+    pub fn new_with_state(rotor_id: &str, pos: usize, ring_loc: usize) -> Self {
+        let tyre = match rotor_id {
+            "I" => &ROTOR_I,
+            "II" => &ROTOR_II,
+            _ => panic!("Invalid rotor identifier {}", rotor_id),
+        };
+
+        Rotor {
+            tyre,
             pos: (pos).rem_euclid(26),
-            ring_loc: (ring_loc).rem_euclid(26),
-            tyre: RotorTyre {
-                 // Rollover when 'Q' is in the "window". Notch is actually aligned with 'Y'
-                notch: 16,
-                alphabet: &ROTOR_1_ALPHABET
-            }
+            ring_loc: (ring_loc).rem_euclid(26)
         }
     }
 
-    fn rotate(&mut self) {
+    pub fn rotate(&mut self) {
         self.pos = (self.pos + 1).rem_euclid(26);
-
-        let x = vec![(1, 2), (3, 4)]
-            .into_iter()
-            .map(|(a, b)| a + b)
-            .collect::<Vec<usize>>();
     }
 }
 
@@ -61,13 +64,13 @@ mod tests {
 
     #[test]
     fn test_rotate() {
-        let mut r = RotorI::new();
+        let mut r = Rotor::new("I");
         assert_eq!(r.pos, 0);
         r.rotate();
         assert_eq!(r.pos, 1);
 
         // Test proper rollover at end of alphabet
-        let mut r = RotorI::new_with_state(25, 0);
+        let mut r = Rotor::new_with_state("I", 25, 0);
         assert_eq!(r.pos, 25);
         r.rotate();
         assert_eq!(r.ring_loc, 0);
@@ -76,7 +79,7 @@ mod tests {
     #[test]
     fn test_rotor_init() {
         // Ring and rotor position values >= 26 should be modulo'd
-        let r = RotorI::new_with_state(26, 26);
+        let r = Rotor::new_with_state("I", 26, 26);
         assert_eq!(r.pos, 0);
         assert_eq!(r.ring_loc, 0);
     }
