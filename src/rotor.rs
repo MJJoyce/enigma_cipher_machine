264065312f48d8fc81d74b582e05d74b9c71aa01
Michael Joyce <mltjoyce@gmail.com>
2022-06-18 22:42:29 -0700

    Fix overly simplistic Rotor mapping
    Rotor mapping was not considering in the "outbound" current flow during
    encoding creating a bi-directional mapping of values. Instead it was
    using the same "wiring" on both inbound and outbound directions.
    
    Rotor related structs have been updated to include a "in" and "out"
    mapping to accommodate this. The mapping API has changes to include a
    post-fixed indicator of which direction the mapping is going. E.g.,
    `map_in` and `map_out`. The "in" direction is from the keyboard to the
    reflector and "out" is from the reflector to the light board.
    
    Rotor mapping now properly considers the rotors location. Previously the
    mapping was only handling the "wiring" of the rotor tyre. The current
    location of the rotor must be considered for a "proper" translation.
    E.g., an 'E' output when the rotor is in position 1 is actually an
    output at position 'D'.
    
    Nearly all the rotor related tests have been updated due to the above
    fixes. A new test has been added to ensure that all the tyre alphabet
    mappings are properly formatted. I.e., a given IN and OUT alphabet
    together form a bi-directional mapping.

--- src/rotor.rs
+++ src/rotor.rs
@@ -1,41 +1,73 @@
 // [E, K, M, F, L, G, D, Q, V, Z, N, T, O, W, Y, H, X, U, S, P, A, I, B, R, C, J]
-const ROTOR_1_ALPHABET: [u8; 26] = [
+const ROTOR_1_ALPHABET_IN: [u8; 26] = [
     4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9,
 ];
 
+const ROTOR_1_ALPHABET_OUT: [u8; 26] = [
+    20, 22, 24, 6, 0, 3, 5, 15, 21, 25, 1, 4, 2, 10, 12, 19, 7, 23, 18, 11, 17, 8, 13, 16, 14, 9,
+];
+
 // [A, J, D, K, S, I, R, U, X, B, L, H, W, T, M, C, Q, G, Z, N, P, Y, F, V, O, E];
-const ROTOR_2_ALPHABET: [u8; 26] = [
+const ROTOR_2_ALPHABET_IN: [u8; 26] = [
     0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4,
 ];
 
+const ROTOR_2_ALPHABET_OUT: [u8; 26] = [
+    0, 9, 15, 2, 25, 22, 17, 11, 5, 1, 3, 10, 14, 19, 24, 20, 16, 6, 4, 13, 7, 23, 12, 8, 21, 18,
+];
+
 // [B, D, F, H, J, L, C, P, R, T, X, V, Z, N, Y, E, I, W, G, A, K, M, U, S, Q, O]
-const ROTOR_3_ALPHABET: [u8; 26] = [
+const ROTOR_3_ALPHABET_IN: [u8; 26] = [
     1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14,
 ];
 
+const ROTOR_3_ALPHABET_OUT: [u8; 26] = [
+    19, 0, 6, 1, 15, 2, 18, 3, 16, 4, 20, 5, 21, 13, 25, 7, 24, 8, 23, 9, 22, 11, 17, 10, 14, 12,
+];
+
 // [E, S, O, V, P, Z, J, A, Y, Q, U, I, R, H, X, L, N, F, T, G, K, D, C, M, W, B]
-const ROTOR_4_ALPHABET: [u8; 26] = [
+const ROTOR_4_ALPHABET_IN: [u8; 26] = [
     4, 18, 14, 21, 15, 25, 9, 0, 24, 16, 20, 8, 17, 7, 23, 11, 13, 5, 19, 6, 10, 3, 2, 12, 22, 1,
 ];
 
+const ROTOR_4_ALPHABET_OUT: [u8; 26] = [
+    7, 25, 22, 21, 0, 17, 19, 13, 11, 6, 20, 15, 23, 16, 2, 4, 9, 12, 1, 18, 10, 3, 24, 14, 8, 5,
+];
+
 // [V, Z, B, R, G, I, T, Y, U, P, S, D, N, H, L, X, A, W, M, J, Q, O, F, E, C, K]
-const ROTOR_5_ALPHABET: [u8; 26] = [
+const ROTOR_5_ALPHABET_IN: [u8; 26] = [
     21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10,
 ];
 
+const ROTOR_5_ALPHABET_OUT: [u8; 26] = [
+    16, 2, 24, 11, 23, 22, 4, 13, 5, 19, 25, 14, 18, 12, 21, 9, 20, 3, 10, 6, 8, 0, 17, 15, 7, 1,
+];
+
 // [J, P, G, V, O, U, M, F, Y, Q, B, E, N, H, Z, R, D, K, A, S, X, L, I, C, T, W]
-const ROTOR_6_ALPHABET: [u8; 26] = [
+const ROTOR_6_ALPHABET_IN: [u8; 26] = [
     9, 15, 6, 21, 14, 20, 12, 5, 24, 16, 1, 4, 13, 7, 25, 17, 3, 10, 0, 18, 23, 11, 8, 2, 19, 22,
 ];
 
+const ROTOR_6_ALPHABET_OUT: [u8; 26] = [
+    18, 10, 23, 16, 11, 7, 2, 13, 22, 0, 17, 21, 6, 12, 4, 1, 9, 15, 19, 24, 5, 3, 25, 20, 8, 14,
+];
+
 // [N, Z, J, H, G, R, C, X, M, Y, S, W, B, O, U, F, A, I, V, L, P, E, K, Q, D, T]
-const ROTOR_7_ALPHABET: [u8; 26] = [
+const ROTOR_7_ALPHABET_IN: [u8; 26] = [
     13, 25, 9, 7, 6, 17, 2, 23, 12, 24, 18, 22, 1, 14, 20, 5, 0, 8, 21, 11, 15, 4, 10, 16, 3, 19,
 ];
 
+const ROTOR_7_ALPHABET_OUT: [u8; 26] = [
+    16, 12, 6, 24, 21, 15, 4, 3, 17, 2, 22, 19, 8, 0, 13, 20, 23, 5, 10, 25, 14, 18, 11, 7, 9, 1,
+];
+
 // [F, K, Q, H, T, L, X, O, C, B, J, S, P, D, Z, R, A, M, E, W, N, I, U, Y, G, V]
-const ROTOR_8_ALPHABET: [u8; 26] = [
+const ROTOR_8_ALPHABET_IN: [u8; 26] = [
     5, 10, 16, 7, 19, 11, 23, 14, 2, 1, 9, 18, 15, 3, 25, 17, 0, 12, 4, 22, 13, 8, 20, 24, 6, 21,
+];
+
+const ROTOR_8_ALPHABET_OUT: [u8; 26] = [
+    16, 9, 8, 13, 18, 0, 24, 3, 21, 10, 1, 5, 17, 20, 7, 12, 2, 15, 11, 4, 22, 25, 19, 6, 23, 14,
 ];
 
 #[derive(Debug)]
@@ -47,55 +79,64 @@ enum RotorTyreNotch {
 #[derive(Debug)]
 pub struct RotorTyre {
     notch: RotorTyreNotch,
-    alphabet: &'static [u8; 26],
+    alphabet_in: &'static [u8; 26],
+    alphabet_out: &'static [u8; 26],
 }
 
 const ROTOR_I: RotorTyre = RotorTyre {
     // Rollover when stepping from 'Q' to 'R' (16 -> 17)
     notch: RotorTyreNotch::Single(16),
-    alphabet: &ROTOR_1_ALPHABET,
+    alphabet_in: &ROTOR_1_ALPHABET_IN,
+    alphabet_out: &ROTOR_1_ALPHABET_OUT,
 };
 
 const ROTOR_II: RotorTyre = RotorTyre {
     // Rollover when stepping from 'E' to 'F' (4 -> 5)
     notch: RotorTyreNotch::Single(4),
-    alphabet: &ROTOR_2_ALPHABET,
+    alphabet_in: &ROTOR_2_ALPHABET_IN,
+    alphabet_out: &ROTOR_2_ALPHABET_OUT,
 };
 
 const ROTOR_III: RotorTyre = RotorTyre {
     // Rollover when stepping from 'V' to 'W' (20 -> 21)
     notch: RotorTyreNotch::Single(20),
-    alphabet: &ROTOR_3_ALPHABET,
+    alphabet_in: &ROTOR_3_ALPHABET_IN,
+    alphabet_out: &ROTOR_3_ALPHABET_OUT,
 };
 
 const ROTOR_IV: RotorTyre = RotorTyre {
     // Rollover when stepping from 'J' to 'K' (8 -> 9)
     notch: RotorTyreNotch::Single(8),
-    alphabet: &ROTOR_4_ALPHABET,
+    alphabet_in: &ROTOR_4_ALPHABET_IN,
+    alphabet_out: &ROTOR_4_ALPHABET_OUT,
 };
 
 const ROTOR_V: RotorTyre = RotorTyre {
     // Rollover when stepping from 'Z' to 'A' (25 -> 0)
     notch: RotorTyreNotch::Single(25),
-    alphabet: &ROTOR_5_ALPHABET,
+    alphabet_in: &ROTOR_5_ALPHABET_IN,
+    alphabet_out: &ROTOR_5_ALPHABET_OUT,
 };
 
 const ROTOR_VI: RotorTyre = RotorTyre {
     // Rollover when stepping from 'Z' to 'A' (25 -> 0) or 'M' to 'N' (11 -> 12)
     notch: RotorTyreNotch::Double([25, 11]),
-    alphabet: &ROTOR_6_ALPHABET,
+    alphabet_in: &ROTOR_6_ALPHABET_IN,
+    alphabet_out: &ROTOR_6_ALPHABET_OUT,
 };
 
 const ROTOR_VII: RotorTyre = RotorTyre {
     // Rollover when stepping from 'Z' to 'A' (25 -> 0) or 'M' to 'N' (11 -> 12)
     notch: RotorTyreNotch::Double([25, 11]),
-    alphabet: &ROTOR_7_ALPHABET,
+    alphabet_in: &ROTOR_7_ALPHABET_IN,
+    alphabet_out: &ROTOR_7_ALPHABET_OUT,
 };
 
 const ROTOR_VIII: RotorTyre = RotorTyre {
     // Rollover when stepping from 'Z' to 'A' (25 -> 0) or 'M' to 'N' (11 -> 12)
     notch: RotorTyreNotch::Double([25, 11]),
-    alphabet: &ROTOR_8_ALPHABET,
+    alphabet_in: &ROTOR_8_ALPHABET_IN,
+    alphabet_out: &ROTOR_8_ALPHABET_OUT,
 };
 
 #[derive(Debug)]
@@ -142,15 +183,25 @@ impl Rotor {
         self.pos = (self.pos + 1).rem_euclid(26);
     }
 
-    pub fn will_step_next_rotor(self) -> bool {
+    pub fn will_step_next_rotor(&self) -> bool {
         match self.tyre.notch {
             RotorTyreNotch::Single(notch_index) => self.pos == notch_index,
             RotorTyreNotch::Double([notch1, notch2]) => self.pos == notch1 || self.pos == notch2,
         }
     }
 
-    pub fn map(&self, input_val: u8) -> u8 {
-        self.tyre.alphabet[(input_val + self.pos + self.ring_loc).rem_euclid(26) as usize]
+    pub fn map_in(&self, input_val: u8) -> u8 {
+        let trans = self.tyre.alphabet_in
+            [(input_val + self.pos + self.ring_loc).rem_euclid(26) as usize]
+            as i16;
+        (trans - self.pos as i16).rem_euclid(26) as u8
+    }
+
+    pub fn map_out(&self, input_val: u8) -> u8 {
+        let trans = self.tyre.alphabet_out
+            [(input_val + self.pos + self.ring_loc).rem_euclid(26) as usize]
+            as i16;
+        (trans - self.pos as i16).rem_euclid(26) as u8
     }
 }
 
@@ -220,61 +271,122 @@ mod tests {
     }
 
     #[test]
+    fn test_verify_alphabet_mappings() {
+        let rotors = vec![
+            ROTOR_I, ROTOR_II, ROTOR_III, ROTOR_IV, ROTOR_V, ROTOR_VI, ROTOR_VII, ROTOR_VIII,
+        ];
+
+        for rotor in rotors {
+            for i in 0usize..rotor.alphabet_in.len() {
+                assert_eq!(
+                    i,
+                    rotor.alphabet_out[rotor.alphabet_in[i] as usize] as usize
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_sample_mapping() {
+        let mut r = Rotor::new("I");
+        assert_eq!(r.map_in(0), b'E' - b'A');
+        assert_eq!(r.map_out(b'E' - b'A'), 0);
+        r.rotate();
+        assert_eq!(r.map_in(0), b'J' - b'A');
+        assert_eq!(r.map_out(b'J' - b'A'), 0);
+        r.rotate();
+        assert_eq!(r.map_in(0), b'K' - b'A');
+        assert_eq!(r.map_out(b'K' - b'A'), 0);
+        r.rotate();
+        assert_eq!(r.map_in(0), b'C' - b'A');
+        assert_eq!(r.map_out(b'C' - b'A'), 0);
+    }
+
+    #[test]
     fn test_mapping_basic() {
         // Check that our mapping calculation handles the simple case
         // at the beginning of an alphabet.
         // Rotor1: E, K, M, F
         let mut r = Rotor::new("I");
-        assert_eq!(r.map(0), b'E' - b'A');
+        assert_eq!(r.map_in(0), b'E' - b'A');
         r.rotate();
-        assert_eq!(r.map(0), b'K' - b'A');
+        assert_eq!(r.map_in(0), b'K' - b'A' - r.pos);
         r.rotate();
-        assert_eq!(r.map(0), b'M' - b'A');
+        assert_eq!(r.map_in(0), b'M' - b'A' - r.pos);
         r.rotate();
-        assert_eq!(r.map(0), b'F' - b'A');
+        assert_eq!(r.map_in(0), b'F' - b'A' - r.pos);
         r.rotate();
 
         // Check that our ring adjustment is shifting the alphabet as expected
         // Rotor1 with ring_loc 1 shifts our answers 'up' 1
         // Rotor1: K, M, F
         let mut r = Rotor::new_with_state("I", 0, 1);
-        assert_eq!(r.map(0), b'K' - b'A');
+        assert_eq!(r.map_in(0), b'K' - b'A' - r.pos);
         r.rotate();
-        assert_eq!(r.map(0), b'M' - b'A');
+        assert_eq!(r.map_in(0), b'M' - b'A' - r.pos);
         r.rotate();
-        assert_eq!(r.map(0), b'F' - b'A');
+        assert_eq!(r.map_in(0), b'F' - b'A' - r.pos);
         r.rotate();
     }
 
     #[test]
     fn test_mapping_rollover() {
-        // Check our mapping when we need to rollover to the start of our
-        // rings alphabet.
+        // Check our mapping when we need to rollover to the start of our rings alphabet.
         let mut r = Rotor::new_with_state("I", 22, 0);
         r.rotate();
-        assert_eq!(r.map(0), b'R' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'R' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'C' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'C' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'J' - b'A');
-        // We rollover here
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'J' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
+        // We rollover here     (           )
         r.rotate();
-        assert_eq!(r.map(0), b'E' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'E' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'K' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'K' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
 
         // The above should be the same if we adjust the ring position as well
         let mut r = Rotor::new_with_state("I", 0, 22);
         r.rotate();
-        assert_eq!(r.map(0), b'R' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'R' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'C' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'C' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'J' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'J' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         // We rollover here
         r.rotate();
-        assert_eq!(r.map(0), b'E' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'E' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
         r.rotate();
-        assert_eq!(r.map(0), b'K' - b'A');
+        assert_eq!(
+            r.map_in(0) as i16,
+            ((b'K' - b'A') as i16 - r.pos as i16).rem_euclid(26)
+        );
     }
 }
